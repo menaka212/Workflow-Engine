03ad3ab009ecfff27d5c19f11d0157f11b91c051
menaka212 <menakamanavalan2@gmail.com>
2026-03-20 15:09:28 +0530

    fixed build path

--- workflow_engine/workflow_engine/settings.py
+++ workflow_engine/workflow_engine/settings.py
@@ -20,12 +20,12 @@ import os
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_uf39sg-o370*4cs%6^bvyu^^qj6(mpg=@wut2^ln05!4y*!b='
+SECRET_KEY =  os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
